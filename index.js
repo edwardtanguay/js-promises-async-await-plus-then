7dd033589edf91eac5a59da8ebc82e5ee7158491
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-04-06 08:10:17 +0200

    fixed time

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ const startElapsedTime = () => {
 };
 
 const getElapsedTime = (timer) => {
-	return `${process.hrtime(timer)[0]}.${process.hrtime(timer)[1]} seconds`;
+	const padLeft = (num, size) => {
+		num = num.toString();
+		while (num.length < size) num = "0" + num;
+		return num;
+	}
+	const ms = parseInt(process.hrtime(timer)[1] / 1000000);
+	const paddedMs = padLeft(ms, 3);
+	return `${process.hrtime(timer)[0]}.${paddedMs} seconds`;
 }
 
 // GET DATA
